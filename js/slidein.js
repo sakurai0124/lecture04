b1f837ec4e5ed6c4cb211d21759c28791a551092
sakurai0124 <s13388ns@sfc.keio.ac.jp>
2014-05-01 16:12:15 +0900

    Update slidein.js

--- js/slidein.js
+++ js/slidein.js
@@ -2,6 +2,7 @@ var endSlideinFoxkeh = function(){
 	var foxkeh = document.querySelector("#slidein-foxkeh img");
 <<<<<<< HEAD
 	foxkeh.setAttribute("class", "slidein");
+	//foxkeh()のclassをslideinに変更
 =======
 	foxkeh.setAttribute("class", "");
 };
@@ -11,8 +12,14 @@ var startSlideinFoxkeh = function(){
 	foxkeh.setAttribute("class", "slidein");
 
 	foxkeh.addEventListener("animationend", endSlideinFoxkeh);
+	//イベントハンドラの登録：endSlideinFoxkeh　イベント、要素：animationend, foxkeh
+	
 >>>>>>> origin/make_slidein_retriable
 };
 
 var slideinFoxkehButton = document.querySelector("#slidein-foxkeh button");
+//イベントハンドラの対象を取得　
 slideinFoxkehButton.addEventListener("click", startSlideinFoxkeh);
+//イベントハンドラの登録：startSlideinFoxkeh　イベント、要素：click, slideinFoxkehButton (#slidein-foxkeh button)
+//ボタンがクリックされたらstartSlideinFoxkehが実行
+
